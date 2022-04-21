fa001d1716f2a42866981e56d6bdf517b9ccfc87
Guido Meoli <gmeoli@e3r1p6.42roma.it>
2022-04-21 16:57:59 +0200

    first_circle

---
+++ 42/get_next_line/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmeoli <gmeoli@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/04 18:14:38 by gmeoli            #+#    #+#             */
+/*   Updated: 2022/02/05 19:08:04 by gmeoli           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+int		ft_strlen(char *file);
+char	*get_line(int fd, char *save);
+void	*ft_calloc(size_t count, size_t size);
+int		ft_strchr(const char *s, char c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_save(char *save, char *file);
+
+#endif
