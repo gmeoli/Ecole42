b301a60bb9ec818df2a30bd651e9ce09ecfb7e6e
gmeoli <guidomeoli@yahoo.it>
2022-11-10 17:56:36 +0100

    Exam_03

--- exam_03/mini_paint/my_mini_paint.c
+++ exam_03/mini_paint/my_mini_paint.c
@@ -3,4 +3,25 @@
 #include <stdlib.h>
 #include <math.h>
 
+int ft_strlen(const char *str)
+{
+	int	i;
 
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int ft_print_error(const char *str, int ret)
+{
+	write(1, str, ft_strlen(str));
+	return (ret);
+}
+
+int main(int ac, char **av)
+{
+	free(drawing);
+	fclose(fd);
+	return (0);
+}
