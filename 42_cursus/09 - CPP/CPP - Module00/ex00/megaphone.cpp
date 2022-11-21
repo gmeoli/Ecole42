8e13a3074ff1cb9cd03287935f413281d947b3e0
gmeoli <guidomeoli@yahoo.it>
2022-11-21 18:19:56 +0100

    ex00

--- 42_cursus/09 - CPP/CPP - Module00/ex00/megaphone.cpp
+++ 42_cursus/09 - CPP/CPP - Module00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: gmeoli <gmeoli@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 19:34:51 by gmeoli            #+#    #+#             */
-/*   Updated: 2022/11/21 17:28:36 by gmeoli           ###   ########.fr       */
+/*   Updated: 2022/11/21 18:18:23 by gmeoli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,12 @@
 int	main(int ac, char **av){
 
  	if (ac == 1){
-	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return 0;
 	}
-	for (size_t i = 0; i < strlen(av[1]); i++){
-		putchar(toupper(av[1][i]));
+		// for (i=0,j=0; i<x, j<y; i++, j--)... è solo un esempio
+	for (size_t i = 1, j = 0; j < strlen(av[1]); i++, j++){
+		putchar(toupper(av[j][i]));
 	}
 	return 0;
 }
