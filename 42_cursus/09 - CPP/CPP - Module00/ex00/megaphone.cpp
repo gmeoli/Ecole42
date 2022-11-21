bffad140660c86212c9acb8c65c44434d2bbdc73
gmeoli <guidomeoli@yahoo.it>
2022-11-21 17:28:51 +0100

    ex00

--- 42_cursus/09 - CPP/CPP - Module00/ex00/megaphone.cpp
+++ 42_cursus/09 - CPP/CPP - Module00/ex00/megaphone.cpp
@@ -6,7 +6,21 @@
 /*   By: gmeoli <gmeoli@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 19:34:51 by gmeoli            #+#    #+#             */
-/*   Updated: 2022/11/18 19:34:53 by gmeoli           ###   ########.fr       */
+/*   Updated: 2022/11/21 17:28:36 by gmeoli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <cstring>
+
+int	main(int ac, char **av){
+
+ 	if (ac == 1){
+	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	for (size_t i = 0; i < strlen(av[1]); i++){
+		putchar(toupper(av[1][i]));
+	}
+	return 0;
+}
