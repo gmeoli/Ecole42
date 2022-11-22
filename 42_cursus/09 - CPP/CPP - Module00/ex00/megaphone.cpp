bd9285800cdb0baf646e3daefa45417d825b0874
gmeoli <guidomeoli@yahoo.it>
2022-11-22 15:46:37 +0100

    ex00

--- 42_cursus/09 - CPP/CPP - Module00/ex00/megaphone.cpp
+++ 42_cursus/09 - CPP/CPP - Module00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: gmeoli <gmeoli@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 19:34:51 by gmeoli            #+#    #+#             */
-/*   Updated: 2022/11/22 15:22:22 by gmeoli           ###   ########.fr       */
+/*   Updated: 2022/11/22 15:46:12 by gmeoli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,8 @@ int	main(int ac, char **av)
 		return 0;
 	}
 	for (int i = 1; i < ac; i++)
-	{
 		for (int j = 0; av[i][j]; j++)
 			std::cout << (char)toupper(av[i][j]);
-	}
 	std::cout << std::endl;
 	return 0;
 }
