54f6601712725ea7ecee4a925645e91b059d49b6
gmeoli <guidomeoli@yahoo.it>
2022-12-04 17:23:39 +0100

    Exam04

--- 42_cursus/Exam_04/microshell.c
+++ 42_cursus/Exam_04/microshell.c
@@ -2,8 +2,7 @@
 #include <sys/wait.h>
 #include <string.h>
 
-int	error(char *str, char *err)
-{
+int	error(char *str, char *err){
 	while (*str)
 		write(2, str++, 1);
 	if (err)
@@ -13,8 +12,7 @@ int	error(char *str, char *err)
 	return (1);
 }
 
-int	exec(char **args, int len, int fd, char **env)
-{
+int	exec(char **args, int len, int fd, char **env){
 	args[len] = NULL;
 	dup2(fd, 0);
 	close(fd);
@@ -22,60 +20,49 @@ int	exec(char **args, int len, int fd, char **env)
 	return (error("error: cannot execute ", args[0]));
 }
 
-int	main(int argc, char **argv, char **env)
-{
-	int	i;
+int	main(int ac, char **av, char **env){
+	int	i = 0;
 	int	pid;
-	int	tmp;
+	int	tmp = dup(0);
 	int	fd[2];
-
-	i = 0;
-	pid = 0;
-	tmp = dup(0);
-	(void)argc;
-	while (argv[i] && argv[i + 1])
+	(void)ac;
+	
+	while (av[i] && av[i + 1])
 	{
-		argv = &argv[i + 1];
+		av = &av[i + 1];
 		i = 0;
-		while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
+		while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
 			i++;
-		if (!strcmp(argv[0], "cd"))
-		{
+		if (!strcmp(av[0], "cd")){
 			if (i != 2)
 				error("error: cd: bad arguments", NULL);
-			else if (chdir(argv[1]) != 0)
-				error("error: cd: cannot change directory to ", argv[1]);
+			else if (chdir(av[1]) != 0)
+				error("error: cd: cannot change directory to ", av[1]);
 		}
-		else if (i != 0 && (argv[i] == NULL || !strcmp(argv[i], ";")))
-		{
+		else if (i != 0 && (av[i] == NULL || !strcmp(av[i], ";"))){
 			pid = fork();
-			if (!pid)
-			{
-				if (exec(argv, i, tmp, env))
+			if (!pid) {
+				if (exec(av, i, tmp, env))
 					return (1);
 			}
-			else
-			{
+			else {
 				close(tmp);
 				while (waitpid(-1, NULL, WUNTRACED) != -1)
 					;
 				tmp = dup(0);
 			}
 		}
-		else if (i != 0 && !strcmp(argv[i], "|"))
-		{
+		else if (i != 0 && !strcmp(av[i], "|")){
 			pipe(fd);
 			pid = fork();
-			if (!pid)
-			{
+			if (!pid){
 				dup2(fd[1], 1);
 				close(fd[1]);
 				close(fd[0]);
-				if (exec(argv, i, tmp, env))
+				if (exec(av, i, tmp, env))
 					return (1);
 			}
-			else
-			{
+			else {
 				close(tmp);
 				close(fd[1]);
 				tmp = fd[0];
