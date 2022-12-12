f4706ef9a547489e19f3f7e53d4ba3eae81d4cee
gmeoli <guidomeoli@yahoo.it>
2022-12-12 13:22:50 +0100

    Ex02

--- 42_cursus/09 - CPP/CPP - Module00/ex02/Account.cpp
+++ 42_cursus/09 - CPP/CPP - Module00/ex02/Account.cpp
@@ -1,20 +1,30 @@
 #include "Account.hpp"
+#include <iostream>
+#include <time.h>
 
-////////////* PUBLIC *////////////
-int	Account::getNbAccounts( void ){
-	return ;
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+///////////////////////////////////
+//------------ PUBLIC -----------//
+///////////////////////////////////
+
+int Account::getNbAccounts( void ){
+	return _nbAccounts;
 }
 
 int	Account::getTotalAmount( void ){
-	return ;
+	return _totalAmount;
 }
 
 int	Account::getNbDeposits( void ){
-	return ;
+	return _totalNbDeposits;
 }
 
 int	Account::getNbWithdrawals( void ){
-	return ;
+	return _totalNbWithdrawals;
 }
 
 void	Account::displayAccountsInfos( void ){
@@ -22,6 +32,11 @@ void	Account::displayAccountsInfos( void ){
 }
 
 Account::Account( int initial_deposit ){
+	(void)initial_deposit;
+	this->_accountIndex = 0;
+	this->_amount = 0;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
 	return ;
 }
 
@@ -30,24 +45,34 @@ Account::~Account(void){
 }
 
 void	Account::makeDeposit( int deposit ){
+	(void)deposit;
 	return ;
 }
 
 bool	Account::makeWithdrawal( int withdrawal ){
-	return ;
+	(void)withdrawal;
+	return true;
 }
 
 int		Account::checkAmount( void ) const {
-	return ;
+	return 0;
 }
 
 void	Account::displayStatus( void ) const {
+	time_t		t = time(0);
+	struct tm	*now = localtime(&t);
+
+	std::cout << "[" << now->tm_year + 1900 << now->tm_mon + 1 << now->tm_mday << "_" << now->tm_hour << now->tm_min << now->tm_sec << "] \n";
 	return ;
 }
 
-////////////* PRIVATE *////////////
+
+///////////////////////////////////
+//------------ PRIVATE ----------//
+///////////////////////////////////
+
 void	Account::_displayTimestamp( void ){
-	return ;
+	std:: cout << "Timestamp" << std::endl;
 }
 
 Account::Account(void){
