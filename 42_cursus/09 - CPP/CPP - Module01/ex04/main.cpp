50b6b5fbf8c8f6caf5e895c3abd4206a857b2bd3
gmeoli <guidomeoli@yahoo.it>
2023-01-10 18:48:45 +0100

    CPP01

--- 42_cursus/09 - CPP/CPP - Module01/ex04/main.cpp
+++ 42_cursus/09 - CPP/CPP - Module01/ex04/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <thread>
 
 void fac_replacestring(std::string &str, const std::string &s1, const std::string &s2) {
 	size_t i = 0, j = 0, end;
@@ -31,7 +30,7 @@ int main(int ac, char **av) {
 	}
 	std::ifstream file(av[1]);
 	if (!file.is_open()) {
-		std::cerr << "File corrupted\n";
+		std::cerr << "File corrupted!\n";
 		return 1;
 	}
 	std::ofstream oFile((std::string)av[1] + ".replace");
@@ -40,10 +39,10 @@ int main(int ac, char **av) {
 		return 1;
 	}
 	while (std::getline(file, line)) {
-	
 		fac_replacestring(line, av[2], av[3]);
 		oFile << line << std::endl;
 	}
+	std::cout << line;
 	oFile.close();
 	file.close();
 	return 0;
