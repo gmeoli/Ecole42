cb0de10fb1ba59d6d40b80cbd69659d13e6fc1ab
gmeoli <guidomeoli@yahoo.it>
2023-03-02 17:49:57 +0100

    CPP08

--- 42_cursus/09 - CPP/CPP - Module08/ex01/main.cpp
+++ 42_cursus/09 - CPP/CPP - Module08/ex01/main.cpp
@@ -2,11 +2,22 @@
 
 int main() {
 	Span sp = Span(5);
-	sp.addNumber(6);
-	sp.addNumber(3);
-	sp.addNumber(17);
-	sp.addNumber(9);
-	sp.addNumber(11);
+
+	try {
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+		// sp.addNumber(1313);
+		// sp.addNumber(43);
+		// sp.addNumber(67);
+	}
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+
 	try {
 		std::cout << sp.shortestSpan() << std::endl;
 	}
