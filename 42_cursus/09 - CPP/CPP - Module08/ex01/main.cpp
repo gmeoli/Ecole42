5501b121fd085df05bd8d2dda0bc91c032650d6f
gmeoli <guidomeoli@yahoo.it>
2023-03-03 16:19:57 +0100

    CPP08

--- 42_cursus/09 - CPP/CPP - Module08/ex01/main.cpp
+++ 42_cursus/09 - CPP/CPP - Module08/ex01/main.cpp
@@ -1,14 +1,22 @@
 	#include "Span.hpp"
 
 int main() {
-	Span sp = Span(5);
+	Span sp = Span(20);
+	std::srand(std::time(NULL)); // Inizializza il generatore di numeri casuali
 
 	try {
-		sp.addNumber(6);
-		sp.addNumber(3);
-		sp.addNumber(17);
-		sp.addNumber(9);
-		sp.addNumber(11);
+		for (unsigned int i = 0; i < 20; i++)
+			sp.addNumber(std::rand()); // Riempie il vettore con numeri casuali
+		for (size_t num : sp.getVector()) // Ciclo "for-range" che stampa i numeri del vettore
+        	std::cout << num << " ";
+		std::cout << std::endl;
+		// sp.addNumber(6);
+		// sp.addNumber(6);
+		// sp.addNumber(6);
+		// sp.addNumber(6);
+		// sp.addNumber(17);
+		// sp.addNumber(9);
+		// sp.addNumber(11);
 		// sp.addNumber(1313);
 		// sp.addNumber(43);
 		// sp.addNumber(67);
