0cd81401262d69bd25f715c650c78f6d683a3218
gmeoli <guidomeoli@yahoo.it>
2023-03-14 23:52:54 +0100

    CPP09

--- 42_cursus/09 - CPP/CPP - Module09/ex00/BitcoinExchange.cpp
+++ 42_cursus/09 - CPP/CPP - Module09/ex00/BitcoinExchange.cpp
@@ -1,3 +1,17 @@
 #include "BitcoinExchange.hpp"
 
+BitcoinExchange::BitcoinExchange() {}
 
+BitcoinExchange::BitcoinExchange(BitcoinExchange const &copy) { *this = copy;}
+
+BitcoinExchange &BitcoinExchange::operator=(BitcoinExchange const &rhs) {
+	if (this == &rhs)
+		return *this;
+	return *this;
+}
+
+BitcoinExchange::~BitcoinExchange() {}
+
+BitcoinExchange::BitcoinExchange(const std::string &filename) {
+
+}
