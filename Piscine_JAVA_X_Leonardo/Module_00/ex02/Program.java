bceb3591354fe1a4869b15ed910484536c16d9ec
gmeoli <guidomeoli@yahoo.it>
2023-04-08 19:18:37 +0200

    Ok

--- Piscine_JAVA_X_Leonardo/Module_00/ex02/Program.java
+++ Piscine_JAVA_X_Leonardo/Module_00/ex02/Program.java
@@ -1,23 +1,21 @@
 import java.util.Scanner;
 
 public class Program {
-	public static int isPrime(int sqrt, int iter) {
-		int temp = 0;
-
-		for (int i = 2; i <= sqrt; i++) {
-			iter++;
-			if (temp % i == 0)
+	public static int isPrime(int sqrt, int input) {
+		for (int i = 2; i <= sqrt + 1; i++) {
+			if (input % i == 0)
 				return 1;
 			}
 		return 0;
 	}
 	public static void main(String[] args) {
 		
-		int temp = 0, iter = 0, sqrt = 0, j = 1;
+		int temp = 0, iter = 0, sqrt = 0, j = 1, input2 = 0;
 
 		while (true) {
 			Scanner scanner = new Scanner(System.in);
 			int	input = scanner.nextInt();
+			input2 = input;
 
 			if (input == 42) {
 				System.out.println("Count of coffee-request - " + iter);
@@ -48,7 +46,7 @@ public class Program {
 				j++;
 			}
 
-			iter += isPrime(sqrt, iter);
+			iter += isPrime(sqrt, input2);
 		}
 	}
 }
