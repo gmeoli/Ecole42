29716df41e1638d6b10d45e56607fd69dea1c4fa
gmeoli <guidomeoli@yahoo.it>
2023-04-17 18:40:28 +0200

    exam05

---
+++ Cursus_C_C++/Exam_05/cpp_module_02/ATarget.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+#include "ASpell.hpp"
+
+class ASpell;
+
+class ATarget
+{
+    private:
+        std::string type;
+    
+    public:
+        ATarget();
+        ATarget(std::string const &type);
+        ATarget(ATarget const &other);
+        ATarget &operator=(ATarget const &other);
+        virtual ~ATarget();
+
+        std::string const &getType() const;
+
+        void getHitBySpell(ASpell const &aspell_ref) const;
+
+        virtual ATarget *clone() const = 0;
+};
